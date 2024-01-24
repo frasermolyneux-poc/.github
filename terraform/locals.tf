12887e57268604370db8308011750d0c467666cc
Fraser Molyneux <frasermolyneux@mx-mail.io>
2024-01-24 20:13:44 +0000

    Adding environments

--- terraform/locals.tf
+++ terraform/locals.tf
@@ -1,5 +1,6 @@
 locals {
   json_files = [for f in fileset("projects", "*.json") : jsondecode(file("projects/${f}"))]
+
   projects = [for content in local.json_files : {
     name = content.name
     github = lookup(content, "github", null) != null ? {
@@ -11,6 +12,19 @@ locals {
       has_issues    = lookup(content.github, "has_issues", false)
       has_projects  = lookup(content.github, "has_projects", false)
       has_wiki      = lookup(content.github, "has_wiki", false)
-    } : null
+    } : null,
+    environments = [for environment in lookup(content, "environments", []) : {
+      name = environment.name
+    }]
   }]
+
+  project_environments = flatten([
+    for project in local.projects : [
+      for environment in project.environments : {
+        key         = format("%s-%s", project.name, environment.name)
+        project     = project.name
+        environment = environment.name
+      }
+    ]
+  ])
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,7 @@
 output "projects" {
   value = local.projects
 }
+
+output "project_environments" {
+  value = local.project_environments
+}
